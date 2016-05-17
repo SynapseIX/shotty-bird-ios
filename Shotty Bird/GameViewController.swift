64841b166a0275cfa0245eb694243da31559af6f
Jorge Tapia <jorge@profapps.co>
2016-05-17 04:56:15 -0400

    Fix issue that caused some flickering when faster birds that have the same z position go over slower birds.

--- Shotty Bird/GameViewController.swift
+++ Shotty Bird/GameViewController.swift
@@ -24,7 +24,7 @@ class GameViewController: UIViewController {
         //skView.showsPhysics = true
         
         /* Sprite Kit applies additional optimizations to improve rendering performance */
-        skView.ignoresSiblingOrder = true
+        skView.ignoresSiblingOrder = false
         
         /* Set the scale mode to scale to fit the window */
         scene.scaleMode = .AspectFill
